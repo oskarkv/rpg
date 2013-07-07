b151e9969db03642d6a44c76b7f65e5e481baec6
Oskar Kvist <oskar.kvist@gmail.com>
2013-07-07 05:19:02 +0200

    Add KryoNet to dependencies

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject game "0.1.0-SNAPSHOT"
   :description "An MMORPG."
-  :dependencies [[org.clojure/clojure "1.5.1"])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojars.miguno/kryonet-all "2.18"]])
