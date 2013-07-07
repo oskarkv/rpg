c02beda020e04379d9266f7481e46455644f8d3b
Oskar Kvist <oskar.kvist@gmail.com>
2013-07-07 05:20:21 +0200

    Add jMonkeyEngine 3.0.1-SNAPSHOT to dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject game "0.1.0-SNAPSHOT"
   :description "An MMORPG."
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojars.miguno/kryonet-all "2.18"]])
+                 [org.clojars.miguno/kryonet-all "2.18"]
+                 [org.clojars.oskarkv/jmonkeyengine "3.0.1-SNAPSHOT"]])
