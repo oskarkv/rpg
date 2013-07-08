ce66219ed75a8b22f6a380a066ed87fb4e134d23
Oskar Kvist <oskar.kvist@gmail.com>
2013-07-08 04:22:46 +0200

    Add filesystem impl. of DataStore protocol

---
+++ src/game/data_store/implementations/filesystem_store.clj
@@ -0,0 +1,17 @@
+(ns game.data-store.implementations.filesystem-store
+  (:require [game.data-store.core :as data-store]
+            [game.core :as core]))
+
+(deftype FilesystemStore [dir]
+  ; Since it is concievable that some stores will need to be started and
+  ; stopped, e.g. a database-backed store, all stores must implement the
+  ; Lifecycle protocol, since code using the store should not have to know
+  ; whether or not a particular store actually needs to be started and stopped.
+  core/Lifecycle
+  (start [this])
+  (stop [this])
+  data-store/DataStore
+  (store [this key val]
+    (spit (str dir "/" key) (pr-str val)))
+  (load [this key]
+    (read-string (slurp (str dir "/" key)))))
