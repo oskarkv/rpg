53ff272520470893ecbd3a6f023efda6a7667e7f
Oskar Kvist <oskar.kvist@gmail.com>
2013-07-10 19:17:56 +0200

    Make error-printing-future print in Vim, not cmd

--- src/game/utils.clj
+++ src/game/utils.clj
@@ -1,4 +1,6 @@
 (ns game.utils)
 
 (defmacro error-printing-future [& body]
-  `(future (try ~@body (catch Exception ~'e (.printStackTrace ~'e)))))
+  ; *err* makes the stack trace print in Vim instead of in the lein repl cmd
+  ; window, when running from Vim.
+  `(future (try ~@body (catch Exception ~'e (.printStackTrace ~'e *err*)))))
