37a06055fb9027b163edc9f95a489c64128a7c0b
Oskar Kvist <oskar.kvist@gmail.com>
2013-07-12 01:29:47 +0200

    Add def- and defmacro- to game.utils

--- src/game/utils.clj
+++ src/game/utils.clj
@@ -4,3 +4,13 @@
   ; *err* makes the stack trace print in Vim instead of in the lein repl cmd
   ; window, when running from Vim.
   `(future (try ~@body (catch Exception ~'e (.printStackTrace ~'e *err*)))))
+
+(defmacro def-
+  "Defines a private var."
+  [name val]
+  (list `def (with-meta name (assoc (meta name) :private true)) val))
+
+(defmacro defmacro-
+  "Same as defmacro but yields a private definition"
+  [name & decls]
+  (list* `defmacro (with-meta name (assoc (meta name) :private true)) decls))
