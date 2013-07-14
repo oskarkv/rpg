eaffc4643b16ba1aabcca0c36cb51c185ccf2abb
Oskar Kvist <oskar.kvist@gmail.com>
2013-07-14 13:20:00 +0200

    Add while-let to game.utils

--- src/game/utils.clj
+++ src/game/utils.clj
@@ -12,6 +12,17 @@
           (when more
             (list* `assert-args more)))))
 
+(defmacro while-let [bindingvec & body]
+  (assert-args
+    (vector? bindingvec) "a vector for its binding"
+    (= 2 (count bindingvec)) "exactly 2 forms in binding vector")
+  (let [form (bindingvec 0) expr (bindingvec 1)]
+    `(loop [result# ~expr]
+       (when result#
+         (let [~form result#]
+           ~@body
+           (recur ~expr))))))
+
 (defmacro error-printing-future [& body]
   ; *err* makes the stack trace print in Vim instead of in the lein repl cmd
   ; window, when running from Vim.
