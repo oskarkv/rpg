5db790a9e311e467f3936a94d974b38e643d997b
Oskar Kvist <oskar.kvist@gmail.com>
2013-07-14 13:27:37 +0200

    Add *warn-on-reflection* false to project.clj

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "An MMORPG."
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojars.miguno/kryonet-all "2.18"]
-                 [org.clojars.oskarkv/jmonkeyengine "3.0.1-SNAPSHOT"]])
+                 [org.clojars.oskarkv/jmonkeyengine "3.0.1-SNAPSHOT"]]
+  :global-vars {*warn-on-reflection* false})
