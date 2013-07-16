22f3805005d9b5dcea8f0c2169974058e4b89a34
Oskar Kvist <oskar.kvist@gmail.com>
2013-07-16 21:23:46 +0200

    Put (dis)connect-msg in vector for consistency

--- src/game/core.clj
+++ src/game/core.clj
@@ -2,18 +2,15 @@
 
 (def port 12345)
 
-(def connect-msg :connect)
-(def disconnect-msg :disconnect)
+(def connect-msg [:connect])
+(def disconnect-msg [:disconnect])
 
-(let [types [connect-msg disconnect-msg :login]]
+(let [types [(connect-msg 0) (disconnect-msg 0) :login]]
   (def types->ints (zipmap types (range)))
   (def ints->types (zipmap (range) types)))
 
 (letfn [(message-transformer [transformer]
-          (fn [msg]
-            (if (vector? msg)
-              (update-in [0] transformer)
-              (transformer msg))))]
+          (fn [msg] (update-in msg [0] transformer)))]
   (def type->int (message-transformer types->ints))
   (def int->type (message-transformer ints->types)))
 
