5f5f4fdfb660d8decddc3eacb9b014ab6f9f96c0
Oskar Kvist <oskar.kvist@gmail.com>
2013-09-16 00:15:08 +0200

    Add game.logging with basic logging

---
+++ src/game/logging.clj
@@ -0,0 +1,23 @@
+(ns game.logging
+  (require [robert.hooke :as rh]
+           [clojure.pprint :as pp]
+           [game.server.core :as sv]
+           [game.common.core-functions :as ccfns]))
+
+(defn print-output [name f & args]
+  (let [result (apply f args)]
+    (println (str "Output of " name ": "))
+    (pp/pprint result)
+    result))
+
+(defmacro make-name-var-list [fn-list]
+  `[~@(for [fn fn-list]
+        [(str fn) `(var ~fn)])])
+
+(def log-output (make-name-var-list [sv/main-update
+                                     ccfns/process-network-msgs]))
+
+(defn add-logging-wrappers []
+  (doseq [[name var] log-output]
+    (rh/clear-hooks var)
+    (rh/add-hook var (partial print-output name))))
