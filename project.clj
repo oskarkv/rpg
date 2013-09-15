6a70cea17dbaa87b20efea30a988da9d10a8760a
Oskar Kvist <oskar.kvist@gmail.com>
2013-09-16 00:05:47 +0200

    Add robert.hooke to deps in project.clj

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "An MMORPG."
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojars.miguno/kryonet-all "2.18"]
-                 [org.clojars.oskarkv/jmonkeyengine "3.0.1-SNAPSHOT"]]
+                 [org.clojars.oskarkv/jmonkeyengine "3.0.1-SNAPSHOT"]
+                 [robert/hooke "1.3.0"]]
   :global-vars {*warn-on-reflection* false})
