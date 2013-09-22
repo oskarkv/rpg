9cef5652dfc9507b831c19c653334fc159c6c051
Oskar Kvist <oskar.kvist@gmail.com>
2013-09-22 03:50:52 +0200

    Add game.logging/print-input

--- src/game/logging.clj
+++ src/game/logging.clj
@@ -2,22 +2,32 @@
   (require [robert.hooke :as rh]
            [clojure.pprint :as pp]
            [game.server.core :as sv]
-           [game.common.core-functions :as ccfns])
+           [game.client.core :as cl]
+           (game.common [core-functions :as ccfns]
+                        [graphics :as gfx]))
   (use [game.utils]))
+
+(defn log-println [name type object]
+  (println (str (current-thread-name) " // " name " " type ":\n"
+                (with-out-str
+                  (pp/pprint object)))))
 
 (defn print-output [name f & args]
   (let [result (apply f args)]
-    (println (str (current-thread-name) " // " name " output:\n"
-                  (with-out-str
-                    (pp/pprint result))))
+    (log-println name "output" result)
     result))
+
+(defn print-input [name f & args]
+    (log-println name "input" args)
+    (apply f args))
 
 (defmacro make-name-var-list [fn-list]
   `[~@(for [fn fn-list]
         [(str fn) `(var ~fn)])])
 
-(def log-output (make-name-var-list [sv/main-update
-                                     ccfns/process-network-msgs]))
+(def log-output (make-name-var-list []))
+
+(def log-input (make-name-var-list []))
 
 (defn add-logging-wrappers []
   (doseq [[name var] log-output]
