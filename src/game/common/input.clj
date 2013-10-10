5a9403e5fb8e183c0c455f54598f1c62fa965a4d
Oskar Kvist <oskar.kvist@gmail.com>
2013-10-10 17:41:22 +0200

    Mod game.common.input to handle mouse buttons

--- src/game/common/input.clj
+++ src/game/common/input.clj
@@ -5,13 +5,23 @@
              MouseInput)
            (com.jme3.input.controls
              KeyTrigger
+             MouseButtonTrigger
              ActionListener)))
 
-(defn string->KeyInput [s]
-  (-> (.getField KeyInput (str "KEY_" (.toUpperCase s))) (.get nil)))
+(defn string->trigger [s]
+  (let [[input trigger prefix s]
+        (cond
+          (.startsWith (.toLowerCase s) "m-")
+          [MouseInput MouseButtonTrigger "BUTTON_" (.substring s 2)]
+          :else
+          [KeyInput KeyTrigger "KEY_" s])
+        key-code (-> (.getField input (str prefix (.toUpperCase s)))
+                     (.get nil))]
+    (eval `(new ~trigger ~key-code))))
 
-(defn create-KeyInputs [key-bindings]
-  (map (fn [[name key type]] [name (string->KeyInput key) type])
+
+(defn create-triggers [key-bindings]
+  (map (fn [[name key type]] [name (string->trigger key) type])
        key-bindings))
 
 (defn create-key-state-map [key-bindings]
@@ -24,7 +34,7 @@
   (swap! key-state-atom assoc :taps []))
 
 (defn start-input [input-manager key-bindings key-state-atom]
-  (let [keyinput-bindings (create-KeyInputs key-bindings)
+  (let [keyinput-bindings (create-triggers key-bindings)
         hold-listener (reify ActionListener
                         (onAction [this name pressed tpf]
                           (swap! key-state-atom assoc (keyword name) pressed)))
@@ -38,8 +48,8 @@
                       (map first (filter #(= type (% 2)) key-bindings))))
         tap-names (filter-fn :tap)
         hold-names (filter-fn :hold)]
-    (doseq [[name key _] keyinput-bindings]
-      (.addMapping input-manager name (into-array [(KeyTrigger. key)])))
+    (doseq [[name trigger _] keyinput-bindings]
+      (.addMapping input-manager name (into-array [trigger])))
     (.addListener input-manager hold-listener hold-names)
     (.addListener input-manager tap-listener tap-names)))
 
