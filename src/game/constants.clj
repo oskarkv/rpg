0f7435d5faa5877c6f60fb7e9d57be6f9dba9279
Oskar Kvist <oskar.kvist@gmail.com>
2013-12-03 08:27:59 +0100

    R def-constants to defconstants for consistency

--- src/game/constants.clj
+++ src/game/constants.clj
@@ -1,12 +1,12 @@
 (ns game.constants
   (:use game.utils))
 
-(defmacro def-constants [& pairs]
+(defmacro defconstants [& pairs]
   (assert-args (even? (count pairs)) "an even number of arguments")
   (when (seq pairs)
   `(do (def ~(with-meta (first pairs) {:const true}) ~(second pairs))
-       (def-constants ~@(drop 2 pairs)))))
+       (defconstants ~@(drop 2 pairs)))))
 
-(def-constants
+(defconstants
   attack-distance 1.5
   attack-delay-leeway 0.3)
