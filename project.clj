b0e365afdb4fa62597db16306e522c0832b2f978
Oskar Kvist <oskar.kvist@gmail.com>
2014-02-13 22:31:46 +0100

    Use kryonet 2.21.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject game "0.1.0-SNAPSHOT"
   :description "An MMORPG."
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojars.miguno/kryonet-all "2.18"]
+                 [org.clojars.oskarkv/kryonet "2.21.0-SNAPSHOT"]
                  [org.clojars.oskarkv/jmonkeyengine "3.0.1-SNAPSHOT"]
                  [robert/hooke "1.3.0"]
                  [org.clojure/data.priority-map "0.0.2"]
