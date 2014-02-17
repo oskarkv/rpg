bfa8f6bfd47832f3a81579e0dc114a5b21dab8b9
Oskar Kvist <oskar.kvist@gmail.com>
2014-02-17 08:30:43 +0100

    Rename table->maps to complete-maps in game.mobs

--- src/game/mobs.clj
+++ src/game/mobs.clj
@@ -5,8 +5,8 @@
    {:name "an orc pawn" :hp 110 :dmg 9}
    {:name "spiderling"}])
 
-(defn table->maps [table]
+(defn complete-maps [table]
   (for [row (next table)]
     (merge (first table) row)))
 
-(def mobs (zipmap (range) (table->maps mobs-table)))
+(def mobs (zipmap (range) (complete-maps mobs-table)))
