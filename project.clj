f3d27953f405cc0b15347d157d3d89676fbb6362
Oskar Kvist <oskar.kvist@gmail.com>
2014-04-03 22:09:35 +0200

    Use JMonkeyEngine 3.0 Stable

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "An MMORPG."
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojars.oskarkv/kryonet "2.21.0-SNAPSHOT"]
-                 [org.clojars.oskarkv/jmonkeyengine "3.0.1-SNAPSHOT"]
+                 [org.clojars.oskarkv/jmonkeyengine "3.0.0"]
                  [robert/hooke "1.3.0"]
                  [org.clojure/data.priority-map "0.0.2"]
                  [org.clojure/math.numeric-tower "0.0.2"]
