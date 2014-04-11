bce7406501e2db683514fe32cd1a5fc874594ccc
Oskar Kvist <oskar.kvist@gmail.com>
2014-04-11 19:32:22 +0200

    Use newer tonegodgui

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                  [robert/hooke "1.3.0"]
                  [org.clojure/data.priority-map "0.0.5"]
                  [org.clojure/math.numeric-tower "0.0.2"]
-                 [tonegodgui/tonegodgui "0.1.0"]]
+                 [tonegodgui/tonegodgui "0.2.0-SNAPSHOT"]]
   :resource-paths ["assets"]
   :jvm-opts ["-XX:-OmitStackTraceInFastThrow"]
   :global-vars {*warn-on-reflection* false})
