aadd1bff2816e821b4deef817fa4aa305639389b
Oskar Kvist <oskar.kvist@gmail.com>
2014-09-08 16:23:04 +0200

    Improve code in sv/process-event :c-move-quantity

--- src/game/server/core.clj
+++ src/game/server/core.clj
@@ -174,7 +174,7 @@
 
 (defmethod process-event :c-move-quantity
   [game-state {:keys [id from-path to-path quantity] :as event}]
-  (let [[from to] (map #(vec (concat [:chars id] %)) [from-path to-path])]
+  (let [[from to] (map #(into [:chars id] %) [from-path to-path])]
     {:new-game-state (ccfns/move-quantity game-state from to quantity)}))
 
 (defmethod process-event :c-quit-looting [game-state {:keys [id ids]}]
