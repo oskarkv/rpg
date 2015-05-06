ef15b0bdf95d43ea48186c23097d2194614a149f
Oskar Kvist <oskar.kvist@gmail.com>
2015-05-06 12:29:40 +0200

    Adjust spawn positions to middle of tiles

--- src/game/game_map.clj
+++ src/game/game_map.clj
@@ -9,15 +9,18 @@
 (defn vectorize [form]
   (walk/postwalk (fn [form] (if (seq? form) (vec form) form)) form))
 
+(defn adjust-pos [pos]
+  (map #(+ 0.5 %) pos))
+
 (defn load-game-map []
   (let [{:keys [terrain spawns start end]} (dg/make-round-rooms 2 10 10 0.45)]
     {:terrain terrain
-     :player-spawn start
+     :player-spawn (adjust-pos start)
      :spawns
      (zipmap
        (range)
        (map
-         (fn [pos] {:pos pos :respawn-time 3
+         (fn [pos] {:pos (adjust-pos pos) :respawn-time 3
                     :mobs [{:mob {:type 0 :levels [1 2]
                                   :drops [{:id 0 :chance 1}
                                           {:id 1 :chance 1 :quantity 3}]}
