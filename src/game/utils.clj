71114bad41cc5bc6048f81f2799598ef432b89e0
Oskar Kvist <oskar.kvist@gmail.com>
2017-09-05 17:05:41 +0200

    Add game.utils/all-set-pairs

--- src/game/utils.clj
+++ src/game/utils.clj
@@ -392,8 +392,17 @@
       (uneven-interleave s2 s1)
       (interleave* s1 s2 (interleave-runs (count s1) (count s2))))))
 
-(defn all-pairs [coll]
-  (loop [pairs [] left coll]
-    (if-let [rst (next left)]
-      (recur (into pairs (map #(vector (first left) %)) rst) (rest left))
-      pairs)))
+(letfn [(all-pairs* [f]
+          (fn [coll]
+            (loop [pairs #{} left coll]
+              (if-let [rst (next left)]
+                (recur (into pairs (map #(f (first left) %)) rst) (rest left))
+                pairs))))]
+  (defn all-pairs
+    "Returns a set of all possible pairs (vectors) of items in coll."
+    [coll]
+    ((all-pairs* vector) coll))
+  (defn all-set-pairs
+    "Returns a set of all possible pairs (sets) of items in coll."
+    [coll]
+    ((all-pairs* hash-set) coll)))
